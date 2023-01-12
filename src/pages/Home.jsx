9ab6aa4dd62a16426260c176aa92fc3302940d39
Silvercrow <maatiasf97@gmail.com>
2023-01-12 18:09:26 -0300

    component services

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Services } from '../components/Services'
 
 export const Home = () => {
     return (
         <>
         <Header />
+        <Services />
         </>
     )
 }
