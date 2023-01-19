309cef4d1e747b555361890f05dd6a02571b67ff
Sebastian Mosquera <sebimosquera@gmail.com>
2023-01-19 13:47:44 -0300

    backend de aintech

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+export const Contact = () => {
+    
+    
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [mensaje, setMensaje] = useState('');
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        try{
+            const data = {
+                nombre,
+                email,
+                mensaje
+            }
+            const response = await axios.post("http://localhost:5000/api/sendemail", data)
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'El email fue enviado con exito!',
+                showConfirmButton: false,
+                timer: 1500
+              })
+              setNombre('')
+              setEmail('')
+              setMensaje('')
+        }catch(err){
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Oops... intenta más tarde.',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+
+    
+    }
+    return (
+        <>
+            <section className="bg-light p-5 wrapper2 d-flex justify-content-center" id="contacto">
+                <div>
+                    <form className=" w-100" onSubmit={handleSubmit}>
+
+                        <h2 className="titles text-center">Contactanos</h2>
+                        <p className="text-light text-center">Envia tu consulta, estamos para ayudarte.</p>
+                        <div className="text-light">
+                            <p>Nombre:</p>
+                            <input className="w-100 borderInputs bg-dark p-2 text-light" type="name" value={nombre} onChange={ (e) => setNombre(e.target.value)} />
+                        </div>
+                        <div className="text-light mt-3">
+                            <p>Email:</p>
+                            <input className="w-100 borderInputs bg-dark p-2 text-light" type="email"  required value={email} onChange={ (e) => setEmail(e.target.value)}  />
+                        </div>
+                        <div className="text-light mt-3">
+                            <p>Tu mensaje:</p>
+                            <textarea type="text" className="w-100 borderInputs bg-dark p-2 text-light" rows="5" cols="50" value={mensaje} onChange={ (e) => setMensaje(e.target.value)} />
+                        </div>
+                        <button className="btn btn-primary text-light w-100" type="submit">Enviar</button>
+                    </form>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ client/src/components/Services.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import landingpage from '../assets/landingpage.png'
+import cartadigital from '../assets/cartadigital.png'
+import system from '../assets/system.png'
+import desarrolloamedida from '../assets/desarrolloamedida.png'
+import eccomerce from '../assets/eccomerce.png'
+
+
+export const Services = () => {
+    return (
+        <>
+            <section className="pt-2 container" id='servicios'  >
+
+                <div className=' mb-5 mt-5 container' data-aos="zoom-in"  data-aos-duration="1500">
+                <h2 className='text-center mb-2'>Nuestros <span className='titles'>Servicios</span></h2>
+                <hr className='hr-titles mb-5' />
+                    <div className='row text-center'>
+                        <div className='col-md-7'>
+                            <div className='row mb-5'>
+                                <div className='col-md-5 mx-5 mb-3 mt-2 shadow border '>
+                                    <img src={landingpage} alt="Landing Page" className="w-100 mt-2"/>
+                                    <h5 className='title-services'>Landing Page</h5>
+                                    <hr className='hr-services' />
+                                    <p className=" text-secondary">El objetivo principal es mantener una marcada presencia en Internet y poder ser encontrado por los buscadores. Tus potenciales clientes podrán conocerte, explorar tus productos/servicios y contactarte.</p>
+                                </div>
+
+                                
+
+                                <div className='col-md-5 mt-2 mb-3 shadow border'>
+                                    <img src={system} alt="Sistema de gestion" className="w-100 mt-2"/>
+                                    <h5 className='title-services'>Sistemas de gestión</h5>
+                                    <hr className='hr-services' />
+                                    <p className=" text-secondary">Creamos sistemas de gestión para adaptar a las nesecidades de tu empresa.</p>
+                                </div>
+
+                                <div className='col-md-5 mx-5 mb-3 mt-2 shadow border'>
+                                    <img src={desarrolloamedida} alt="Desarrollo a medida" className="w-100 mt-2"/>
+                                    <h5 className='title-services'>Desarrollo a medida</h5>
+                                    <hr className='hr-services' />
+                                    <p className=" text-secondary">Desarrollamos software de alto nivel y de la mejor manera a las necesidades de cada cliente.</p>
+                                </div>
+
+                                <div className='col-md-5 mt-2 mb-3 shadow border'>
+                                    <img src={eccomerce} alt="Tienda digital" className="w-75 mt-2"/>
+                                    <h5 className='title-services'>Tienda Online</h5>
+                                    <hr className='hr-services' />
+                                    <p className=" text-secondary">Creamos tu tienda digital </p>
+                                </div>
+
+                                <div className='col-md-5 mx-5 mb-3 mt-2 shadow border '>
+                                    <img src={cartadigital} alt="Carta digital" className="w-100 mt-2"/>
+                                    <h5 className='title-services'>Carta digital</h5>
+                                    <hr className='hr-services' />
+                                    <p className=" text-secondary">Chau papel! creamos tu carta digital + QR</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </section>
+        </>
+    )
+}
